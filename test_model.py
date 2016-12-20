d2772fa9b0ce24f7de44a7a1e6bef0a66ff07bb6
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-12-20 22:08:21 +0400

    Add files via upload

--- test_model.py
+++ test_model.py
@@ -15,11 +15,13 @@ UO = UnaryOperation
 P = Print
 
 
-def is_same(number, expected_value, monkeypatch):
-    monkeypatch.setattr(sys, "stdout", StringIO())
+def get_value(number):
+    old_stdout = sys.stdout
+    sys.stdout = StringIO()
     scope = Scope()
     Print(number).evaluate(scope)
-    return sys.stdout.getvalue() == str(expected_value) + "\n"
+    return int(sys.stdout.getvalue())
+    sys.stdout = old_stdout
 
 
 class TestScope:
@@ -51,21 +53,23 @@ class TestNumber:
     def test_number(self):
         scope = Scope()
         n = Number(43)
-        assert n.evaluate(scope) is n 
+        assert get_value(n) == 43
+        assert n.evaluate(scope) is n
 
 
 class TestPrint:
 
-    def test_print_number(self, monkeypatch):
+    def test_print_number(self):
         n = Number(43)
-        assert is_same(n, 43, monkeypatch)
-    
+        assert get_value(n) == 43
+
     def test_print_evaluate(self, monkeypatch):
         monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
         res = Print(BO(N(30), "+", N(13))).evaluate(scope)
-        assert is_same(res, 43, monkeypatch)
+        assert get_value(res) == 43
         assert sys.stdout.getvalue() == "43\n"
+
 
 class TestRead:
     def test_read(self, monkeypatch):
@@ -75,68 +79,68 @@ class TestRead:
         read_res = Read('a').evaluate(scope)
         Print(R('a')).evaluate(scope)
         assert sys.stdout.getvalue() == "43\n"
-        assert is_same(read_res, 43, monkeypatch)
+        assert get_value(read_res) == 43
 
 
 class TestUnaryOperation:
 
-    def test_unaryoperation_not(self, monkeypatch):
+    def test_unary_operation_not(self):
         scope = Scope()
         n = Number(1)
         not_n = UnaryOperation('!', n).evaluate(scope)
-        assert is_same(not_n, 0, monkeypatch)
+        assert get_value(not_n) == 0
 
-    def test_unaryoperation_minus(self, monkeypatch):
+    def test_unary_operation_minus(self):
         scope = Scope()
         n = Number(43)
-        minus_n = UnaryOperation('-',n).evaluate(scope)
-        assert is_same(minus_n, -43, monkeypatch)
+        minus_n = UnaryOperation('-', n).evaluate(scope)
+        assert get_value(minus_n) == -43
 
-    def test_unaryoperation_evaluate(self, monkeypatch):
+    def test_unary_operation_evaluate(self):
         scope = Scope()
         n = Number(0)
         ev = UnaryOperation('-', UnaryOperation('!', n))
-        assert is_same(ev, -1, monkeypatch)
+        assert get_value(ev) == -1
 
 
 class TestBinaryOperation:
 
-    def test_binaryoperation(self, monkeypatch):
+    def test_binary_operation(self):
         scope = Scope()
         a = Number(43)
         b = Number(10)
         summ = BinaryOperation(a, '+', b)
-        assert is_same(summ, 53, monkeypatch)
+        assert get_value(summ) == 53
         con = BinaryOperation(a, '&&', Number(0))
-        assert is_same(con, 0, monkeypatch)
+        assert get_value(con) == 0
 
-    def test_unaryoperation_evaluate(self, monkeypatch):
+    def test_binary_operation_evaluate(self):
         scope = Scope()
         a = Number(43)
         b = Number(10)
         summ = BinaryOperation(a, '-', b)
         eq = BinaryOperation(summ, '==', Number(33))
-        assert is_same(eq, 1, monkeypatch)
+        assert get_value(eq) == 1
 
 
 class TestFunction:
 
-    def test_function_empty_arguments(self, monkeypatch):
+    def test_function_empty_arguments(self):
         scope = Scope()
         func = Function([], [BO(Number(10), '+', Number(33))])
         res = func.evaluate(scope)
-        assert is_same(res, 43, monkeypatch)
+        assert get_value(res) == 43
 
-    def test_function_many_arguments(self, monkeypatch):
+    def test_function_many_arguments(self):
         scope = Scope()
         scope["a"] = Number(33)
         scope["b"] = Number(10)
-        func = Function(("a","b"),[BO(Reference("a"), '+', Reference("b"))])
+        func = Function(("a", "b"), [BO(Reference("a"), '+', Reference("b"))])
         res = func.evaluate(scope)
-        assert is_same(res, 43, monkeypatch)
+        assert get_value(res) == 43
 
     def test_function_big_body(self, monkeypatch):
-        monkeypatch.setattr(sys,"stdout", StringIO())
+        monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
         scope["a"] = Number(33)
         scope["b"] = Number(10)
@@ -150,13 +154,13 @@ class TestFunction:
     def test_function_empty_body(self, monkeypatch):
         scope = Scope()
         F(('a'), []).evaluate(scope)
-        
+
 
 class TestFunctionDefiniction:
 
     def test_function_definition_simple(self):
         scope = Scope()
-        func = Function([],[BO(Number(10), '+', Number(33))])
+        func = Function([], [BO(Number(10), '+', Number(33))])
         func2 = FD("func", func).evaluate(scope)
         assert scope["func"] is func
         assert func2 is func
@@ -180,30 +184,39 @@ class TestReference:
 
 
 class TestConditional:
+
     def test_conditional_empty(self, monkeypatch):
         monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
-        C(BO(BO(N(3), '%', N(3)), '==', N(0)), [], [Print(N(1))]).evaluate(scope)
-        C(BO(BO(N(3), '%', N(4)), '==', N(0)), [], [Print(N(1))]).evaluate(scope)
+        C(BO(BO(N(3), '%', N(3)), '==', N(0)), [], [P(N(1))]).evaluate(scope)
+        C(BO(BO(N(3), '%', N(4)), '==', N(0)), [], [P(N(1))]).evaluate(scope)
         assert sys.stdout.getvalue() == '1\n'
 
+    def test_conditional_without_if_false(self, monkeypatch):
+        monkeypatch.setattr(sys, "stdout", StringIO())
+        scope = Scope()
+        res1 = C(N(1), [P(N(1))]).evaluate(scope)
+        C(N(0), [P(N(1))]).evaluate(scope)
+        assert sys.stdout.getvalue() == '1\n'
+        assert get_value(res1) == 1
+
     def test_conditional_simple(self, monkeypatch):
-        monkeypatch.setattr(sys,"stdout", StringIO())
+        monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
         C(BO(N(3), '==', N(3)), [P(N(1))], [Print(N(0))]).evaluate(scope)
         C(BO(N(2), '==', N(3)), [P(N(1))], [Print(N(0))]).evaluate(scope)
         assert sys.stdout.getvalue() == '1\n0\n'
 
     def test_conditional_return(self, monkeypatch):
-        monkeypatch.setattr(sys,"stdout", StringIO())
+        monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
         res1 = C(BO(N(3), '==', N(3)), [N(1)], [N(0)]).evaluate(scope)
         res2 = C(BO(N(2), '==', N(3)), [N(1)], [N(0)]).evaluate(scope)
-        assert is_same(res1, 1, monkeypatch)
-        assert is_same(res2, 0, monkeypatch)
+        assert get_value(res1) == 1
+        assert get_value(res2) == 0
 
     def test_conditional_empty2(self, monkeypatch):
-        monkeypatch.setattr(sys,"stdout", StringIO())
+        monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
         C(BO(N(3), '==', N(3)), [P(N(1))], []).evaluate(scope)
         C(BO(N(2), '==', N(3)), [P(N(2))], []).evaluate(scope)
@@ -217,20 +230,17 @@ class TestConditional:
         monkeypatch.setattr(sys, "stdout", StringIO())
         scope = Scope()
         res1 = C(BO(N(3), '==', N(3)),
-          [P(N(1)), P(N(2))],
-          [P(N(3)), P(N(4))]).evaluate(scope)
-        #assert is_same(res1, 2, monkeypatch)
+                 [P(N(1)), P(N(2))],
+                 [P(N(3)), P(N(4))]).evaluate(scope)
         res2 = C(BO(N(2), '==', N(3)),
-          [P(N(1)), P(N(2))],
-          [P(N(3)), P(N(4))]).evaluate(scope)
-        #assert is_same(res2, 4, monkeypatch)
+                 [P(N(1)), P(N(2))],
+                 [P(N(3)), P(N(4))]).evaluate(scope)
         assert sys.stdout.getvalue() == '1\n2\n3\n4\n'
 
 
 class TestFunctionCall:
 
-    def test_FunctionCall_with_simple(self, monkeypatch):
-        monkeypatch.setattr(sys, "stdout", StringIO())
+    def test_function_call_with_simple(self):
         parent = Scope()
         FD('max',
            F(('a', 'b'),
@@ -239,9 +249,9 @@ class TestFunctionCall:
               [R('b')])])).evaluate(parent)
         n = FC(R('max'),
                [Number(6), Number(5)]).evaluate(parent)
-        assert is_same(n, 6, monkeypatch)
+        assert get_value(n) == 6
 
-    def test_FunctionCall_very_tricky(self, monkeypatch):
+    def test_function_call_very_tricky(self):
         parent = Scope()
         FD('strange_max',
            F(('a', 'b'),
@@ -250,12 +260,12 @@ class TestFunctionCall:
                               [R('b'), R('a')])])])).evaluate(parent)
         n = FC(R('strange_max'),
                [Number(6), Number(5)]).evaluate(parent)
-        assert is_same(n, 6, monkeypatch)
+        assert get_value(n) == 6
 
 
 class TestIntegration:
 
-    def test_factorial(self , monkeypatch):
+    def test_factorial(self):
         parent = Scope()
         FD('factorial',
            F(('a'),
@@ -266,9 +276,9 @@ class TestIntegration:
                     '*', R('a'))])])).evaluate(parent)
         n = FC(R('factorial'),
                [Number(5)]).evaluate(parent)
-        assert is_same(n, 120, monkeypatch)
+        assert get_value(n) == 120
 
-    def test_fibonacci(self, monkeypatch):
+    def test_fibonacci(self):
         parent = Scope()
         FD('fib',
            F(('a'),
@@ -280,9 +290,9 @@ class TestIntegration:
                             [BO(R('a'), '-', N(2))]))])])).evaluate(parent)
         n = FC(R('fib'),
                [Number(8)]).evaluate(parent)
-        assert is_same(n, 21, monkeypatch)
+        assert get_value(n) == 21
 
-    def test_average(self, monkeypatch):
+    def test_average(self):
         parent = Scope()
         FD('average',
            F(('a', 'b'),
@@ -290,4 +300,4 @@ class TestIntegration:
                  '/', N(2))])).evaluate(parent)
         n = FC(R('average'),
                [Number(4), Number(6)]).evaluate(parent)
-        assert is_same(n, 5, monkeypatch)
+        assert get_value(n) == 5
