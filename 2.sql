544a8b602c92948d82ab6c086db045fe7b5c6a25
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-12-21 10:32:39 +0300

    Delete 2.sql

--- 2.sql
+++
@@ -1,8 +0,0 @@
-SELECT country.name, rate FROM Country
-JOIN literacyrate ON Country.Code = literacyrate.CountryCode
-where rate = (
-	select max(rate) from literacyrate
-	where countrycode = code
-)
-group by country.name
-order by rate desc
