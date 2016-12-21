de95c87ef96d5531f4df6c34750914098d5a9ed0
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-12-21 18:43:29 +0300

    Update 6.sql

--- SQL/6.sql
+++ SQL/6.sql
@@ -1,4 +1,4 @@
 SELECT city.name as "City", city.population as "City Population",country.population as "Country Population" FROM City
 JOIN Country ON Country.Code = City.CountryCode
-order by cast(city.population as real)/cast(country.population as real) desc
+order by cast(city.population as real)/cast(country.population as real) desc, city.name
 limit 20
