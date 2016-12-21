4139148ca4d183c31f079696e87128d2760ff50e
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-12-21 18:43:13 +0300

    Update 2.sql

--- SQL/2.sql
+++ SQL/2.sql
@@ -1,8 +1,11 @@
 SELECT country.name, rate FROM Country
 JOIN literacyrate ON Country.Code = literacyrate.CountryCode
 where rate = (
-	select max(rate) from literacyrate
+	select rate from literacyrate
 	where countrycode = code
+	order by year desc 
+	limit 1
 )
 group by country.name
 order by rate desc
+limit 1
