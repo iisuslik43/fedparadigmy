1a3ccdb62ec822436358cbdef3811118541f0a92
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-12-28 21:36:59 +0300

    Update computation.h

--- computation/computation.h
+++ computation/computation.h
@@ -7,21 +7,21 @@
 typedef void(*OnComputationComplete)(void*);
 
 struct Computation {
-    struct Task task;
-    int finished;
-    pthread_cond_t signal;
-    pthread_mutex_t m;
-    OnComputationComplete on_complete;
-    void* on_complete_arg;
-    void (*f)(void*);
-    void* arg;
+        struct Task task;
+        int finished;
+        pthread_cond_t signal;
+        pthread_mutex_t m;
+        OnComputationComplete on_complete;
+        void* on_complete_arg;
+        void (*f)(void*);
+        void* arg;
 };
 
 void thpool_submit_computation(
-    struct ThreadPool *pool,
-    struct Computation *computation,
-    OnComputationComplete on_complete,
-    void* on_complete_arg
+        struct ThreadPool *pool,
+        struct Computation *computation,
+        OnComputationComplete on_complete,
+        void* on_complete_arg
 );
 
 void thpool_complete_computation(struct Computation *computation);
