b0fc72d622ceb3a3b57c746b1a2765defce6078d
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-12-28 13:25:27 +0400

    Add files via upload

---
+++ computation/computation.c
@@ -0,0 +1,40 @@
+#include "thread_pool.h"
+#include "Computation.h"
+#include <pthread.h>
+
+void thpool_submit_computation(struct ThreadPool *pool, struct Computation *computation, OnComputationComplete on_complete,
+    	    		       void* on_complete_arg)
+{
+	pthread_mutex_lock(&computation -> m);
+	computation -> on_complete = on_complete;
+	computation -> on_complete_arg = on_complete_arg;
+	computation -> finished = 0;
+	computation -> task.f = computation -> f;
+	computation -> task.arg = computation -> arg;
+	pthread_mutex_init(&computation -> m, NULL);
+	pthread_cond_init(&computation -> signal, NULL);
+	thpool_submit(pool, &computation -> task);
+	pthread_mutex_unlock(&computation -> m);
+}
+
+void thpool_complete_computation(struct Computation *computation)
+{
+	pthread_mutex_lock(&computation -> m);
+	(computation -> on_complete)(computation -> on_complete_arg);
+	computation -> finished = 1;
+	pthread_cond_signal(&computation -> signal);
+    	pthread_mutex_unlock(&computation -> m);
+}
+
+void thpool_wait_computation(struct Computation *computation){
+	pthread_mutex_lock(&computation -> m);
+	while (!computation -> finished)
+	{
+		pthread_cond_wait(&computation -> signal, &computation -> m);
+	}
+	pthread_mutex_unlock(&computation -> m);
+	pthread_mutex_destroy(&computation -> m);
+	pthread_cond_destroy(&computation -> signal);
+	thpool_wait(&computation -> task);	
+
+}
